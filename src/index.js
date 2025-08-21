c0c6a8c46d9faf566b0b567444458feddcca5357
PRAJWAL-D-R <prajwaldr1si23mc037@gmail.com>
2025-08-21 22:18:51 +0530

    Added basic express functionality

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const {serverConfig, logger} = require ('./config')
+const apiRoutes = require ('./routes')
+const app = express();
+
+app.use('/api',apiRoutes);
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`Connected to PORT : ${serverConfig.PORT} Successfully`);
+    //logger.info(`Server running on PORT: ${serverConfig.PORT}`);
+})
